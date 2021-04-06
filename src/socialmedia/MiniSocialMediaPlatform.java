e5017d155f17612148224b06a94ec39b2edde53a
AfnanGurung <agurung.75050@farnborough.ac.uk>
2021-04-06 17:00:24 +0100

    changed stuff test

--- src/socialmedia/MiniSocialMediaPlatform.java
+++ src/socialmedia/MiniSocialMediaPlatform.java
@@ -19,7 +19,7 @@ public interface MiniSocialMediaPlatform extends Serializable {
 
 	/**
 	 * The method creates an account in the platform with the given handle.
-	 * <p>
+	 * <p> test
 	 * The state of this SocialMediaPlatform must be be unchanged if any exceptions
 	 * are thrown.
 	 * 
