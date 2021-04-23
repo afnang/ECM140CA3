f6ca0683ea3aaab0f3a0c967806ac1c6ced7e958
Alperen Türkmen <57950250+AlperenTurkmen@users.noreply.github.com>
2021-04-23 17:27:13 +0100

    Added some tests

--- src/SocialMediaPlatformTestApp.java
+++ src/SocialMediaPlatformTestApp.java
@@ -1,7 +1,11 @@
 import socialmedia.AccountIDNotRecognisedException;
 import socialmedia.BadSocialMedia;
+import socialmedia.HandleNotRecognisedException;
 import socialmedia.IllegalHandleException;
 import socialmedia.InvalidHandleException;
+import socialmedia.InvalidPostException;
+import socialmedia.NotActionablePostException;
+import socialmedia.PostIDNotRecognisedException;
 import socialmedia.SocialMediaPlatform;
 
 /**
@@ -15,7 +19,6 @@ import socialmedia.SocialMediaPlatform;
  * @version 1.0
  */
 public class SocialMediaPlatformTestApp {
-
 	/**
 	 * Test method.
 	 * 
@@ -33,20 +36,53 @@ public class SocialMediaPlatformTestApp {
 		
 		Integer id;
 		try {
-
+			
 			id = platform.createAccount("my_handle");
 			System.out.println(id);
-			assert (platform.getNumberOfAccounts() == 1) : "number of accounts registered in the system does not match";
-
+			platform.createAccount("diogo","Hello World!");
+			platform.createAccount("obama");
+			platform.createAccount("bush");
+			platform.createAccount("Diogo Pacheco has to be more than 30 characters.........","Object Oriented Programming.");
+			//The code above must create an error.
+			platform.createPost("diogo", "Hi everyone, this is the first post.");
+			System.out.println("YEAH:"+ platform.getNumberOfAccounts());
+			//platform.createPost("at", " ");
+			//assert (platform.getNumberOfAccounts() == 1) : "number of accounts registered in the system does not match";
+			platform.endorsePost("obama", 1);
+			platform.endorsePost("obama", 1); //Must create an error.
+			platform.endorsePost("diogo", 1); //It's ok to endorse his own post
+			platform.endorsePost("diogo", 1); //Must create an error.
+			platform.commentPost("obama", 1, "Yes we can");
+			System.out.println("Total comments:"+platform.getTotalCommentPosts());
+			System.out.println("The most endorsed post: "+platform.getMostEndorsedPost());
+			System.out.println("Total original posts: "+platform.getTotalOriginalPosts());
+			System.out.println("Total comments: "+platform.getTotalCommentPosts());
 			platform.removeAccount(id);
-			assert (platform.getNumberOfAccounts() == 0) : "number of accounts registered in the system does not match";
+			
+			platform.showAccount("diogo");
+			platform.showAccount("bush");
+			platform.showAccount("obama");
+			
+			//assert (platform.getNumberOfAccounts() == 0) : "number of accounts registered in the system does not match";
 		} catch (IllegalHandleException e) {
 			assert (false) : "IllegalHandleException thrown incorrectly";
 		} catch (InvalidHandleException e) {
-
+			System.out.println("Invalid handle.");
 			assert (false) : "InvalidHandleException thrown incorrectly";
 		} catch (AccountIDNotRecognisedException e) {
 			assert (false) : "AccountIDNotRecognizedException thrown incorrectly";
+		} catch (HandleNotRecognisedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidPostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (PostIDNotRecognisedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NotActionablePostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		} finally {
 			System.out.println("Finally here.");
 		}
