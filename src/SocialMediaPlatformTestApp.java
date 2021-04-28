88815daf945e42d260716eb40e351ccd0c90262a
Alperen Türkmen <57950250+AlperenTurkmen@users.noreply.github.com>
2021-04-28 13:59:17 +0100

    Works quite well
    added removePost test

--- src/SocialMediaPlatformTestApp.java
+++ src/SocialMediaPlatformTestApp.java
@@ -33,73 +33,69 @@ public class SocialMediaPlatformTestApp {
 		assert (platform.getTotalOriginalPosts() == 0) : "Innitial SocialMediaPlatform not empty as required.";
 		assert (platform.getTotalCommentPosts() == 0) : "Innitial SocialMediaPlatform not empty as required.";
 		assert (platform.getTotalEndorsmentPosts() == 0) : "Innitial SocialMediaPlatform not empty as required.";
-		
-		Integer id;
+
 		try {
 			platform.erasePlatform();
-			id = platform.createAccount("my_handle");
-			platform.createAccount("ddiogo","Hello World!");
-			platform.createAccount("diogo","Hello World!");
-			platform.createAccount("obama","Former president of USA");
-			platform.createAccount("dida","Milan legendary XI");
-			platform.createAccount("maldini","Milan legendary XI");
-			platform.createAccount("nesta","Milan legendary XI");
-			platform.createAccount("stam","Milan legendary XI");
-			platform.createAccount("cafu","Milan legendary XI");
-			platform.createAccount("gattuso","Milan legendary XI");
-			platform.createAccount("seedorf","Milan legendary XI");
-			platform.createAccount("pirlo","Milan legendary XI");
-			platform.createAccount("kaka","Milan legendary XI");
-			platform.createAccount("crespo","Milan legendary XI");
-			platform.createAccount("shevchenko","Milan legendary XI");
+			platform.createAccount("ddiogo", "Hello World!");
+			platform.createAccount("diogo", "Hello World!");
+			platform.createAccount("obama", "Former president of USA");
+			platform.createAccount("dida", "Milan legendary XI");
+			platform.createAccount("maldini", "Milan legendary XI");
+			platform.createAccount("nesta", "Milan legendary XI");
+			platform.createAccount("stam", "Milan legendary XI");
+			platform.createAccount("cafu", "Milan legendary XI");
+			platform.createAccount("gattuso", "Milan legendary XI");
+			platform.createAccount("seedorf", "Milan legendary XI");
+			platform.createAccount("pirlo", "Milan legendary XI");
+			platform.createAccount("kaka", "Milan legendary XI");
+			platform.createAccount("crespo", "Milan legendary XI");
+			platform.createAccount("shevchenko", "Milan legendary XI");
 			platform.createAccount("ronaldinho");
 			platform.createAccount("henry");
 			platform.createAccount("zlatan");
-			System.out.println("YEAH:"+ platform.getNumberOfAccounts());
+			System.out.println("YEAH:" + platform.getNumberOfAccounts());
 
-			
-			
-			//The code above must create an error.
-			platform.createPost("diogo", "This is the first post"); //id 1
-			platform.createPost("kaka", "Thierry Henry would've been a perfect fit"); //id 2
-			platform.createPost("shevchenko", "Ukraine is the best"); //id 3
-			platform.createPost("gattuso", "I'm Gattuso."); //id 4
-			platform.createPost("crespo", "GOOAAALL!!!"); //id 5
-			platform.createPost("pirlo", "spaghetti is the best"); //id 6
-			platform.createPost("crespo", "Is Zlatan better than me?"); //id 7
-			
-			
-			
-			//platform.createPost("at", " ");
-			//assert (platform.getNumberOfAccounts() == 1) : "number of accounts registered in the system does not match";
-			platform.endorsePost("obama", 1); //id 8
-			platform.endorsePost("diogo", 1); //id 9
-			platform.endorsePost("henry", 2); //id 10
-			
-			platform.commentPost("obama", 1, "Yes we can"); //id 11
-			platform.commentPost("obama", 11, "Yes we can again"); //id 12
-			platform.commentPost("obama", 11, "Yes we can again"); //id 12
+			// The code above must create an error.
+			platform.createPost("diogo", "This is the first post"); // id 1
+			platform.createPost("kaka", "Thierry Henry would've been a perfect fit"); // id 2
+			platform.createPost("shevchenko", "Ukraine is the best"); // id 3
+			platform.createPost("gattuso", "I'm Gattuso."); // id 4
+			platform.createPost("crespo", "GOOAAALL!!!"); // id 5
+			platform.createPost("pirlo", "spaghetti is the best"); // id 6
+			platform.createPost("crespo", "Is Zlatan better than me?"); // id 7
 
-			platform.commentPost("obama",12,"commenting on my comment"); //id 14
-			platform.commentPost("pirlo", 4, "I'm Pirlo"); //id 15
-			//platform.commentPost("zlatan", 7, ":D"); //id 16
+			// assert (platform.getNumberOfAccounts() == 1) : "number of accounts registered
+			// in the system does not match";
+			platform.endorsePost("obama", 1); // id 8
+			platform.endorsePost("diogo", 1); // id 9
+			platform.endorsePost("henry", 2); // id 10
 
-			//platform.deletePost(16);
-			System.out.println("No. Comments:"+ platform.getTotalCommentPosts());
-			System.out.println("No. Endorsements:"+ platform.getTotalEndorsmentPosts());
-			System.out.println("No. Original posts:"+ platform.getTotalOriginalPosts());
-			
+			platform.commentPost("obama", 1, "Yes we can"); // id 11
+			platform.commentPost("obama", 11, "Yes we can again"); // id 12
+			platform.commentPost("obama", 11, "Yes we can again"); // id 12
+
+			platform.commentPost("obama", 12, "commenting on my comment"); // id 14
+			platform.commentPost("pirlo", 4, "I'm Pirlo"); // id 15
+			platform.commentPost("zlatan", 7, ":D"); //id 16
+			System.out.println(platform.showIndividualPost(16));
+			platform.commentPost("maldini", 16, "He yarram.");
+			platform.deletePost(16);
+			System.out.println(platform.showIndividualPost(16));
+			System.out.println("No. Comments:" + platform.getTotalCommentPosts());
+			System.out.println("No. Endorsements:" + platform.getTotalEndorsmentPosts());
+			System.out.println("No. Original posts:" + platform.getTotalOriginalPosts());
+
 			platform.changeAccountHandle("pirlo", "tirlo");
-			//System.out.println(platform.showPostChildrenDetails(1));
-			//System.out.println(platform.showPostChildrenDetails(4));
-			//System.out.println(platform.showPostChildrenDetails(7));
+			// System.out.println(platform.showPostChildrenDetails(1));
+			// System.out.println(platform.showPostChildrenDetails(4));
+			// System.out.println(platform.showPostChildrenDetails(7));
 			System.out.println("TestApp worked fine,");
 		} catch (IllegalHandleException e) {
 			e.printStackTrace();
-			//assert (false) : "IllegalHandleException thrown incorrectly";
+			// assert (false) : "IllegalHandleException thrown incorrectly";
 		} catch (InvalidHandleException e) {
 			e.printStackTrace();
-			//assert (false) : "InvalidHandleException thrown incorrectly";
+			// assert (false) : "InvalidHandleException thrown incorrectly";
 
 		} catch (HandleNotRecognisedException e) {
 			// TODO Auto-generated catch block
